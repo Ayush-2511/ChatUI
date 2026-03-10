c5146d9d3bd21620e1fb55c71ec302484f2a6dcf
Ayush-2511 <ayushbhartii2511@gmail.com>
2026-03-10 19:23:00 +0530

    only made animation for now

---
+++ Chatui/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Loading from './components/Loading'
+
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <Loading />
+    </div>
+   
+  )
+}
+
+export default App
---
+++ Chatui/src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import './Loading.css'
+function Loading() {
+    return (
+        <div className="loading">
+            
+            <div className="loadBar-down">
+                <div className="loadBar-up"></div>
+            </div>
+        </div>
+    );
+}
+export default Loading;
